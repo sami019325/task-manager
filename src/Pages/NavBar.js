07b4ad0f7e2159fb3b9a6cc1efe44fc1e4ded786
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-12-28 22:39:29 +0600

    nav bar added

--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './../Native.css'
+import logo from './../resources/Vector 1.png'
 
 const NavBar = () => {
+    const user = 'sami'
     return (
-        <div>
-            Nav
+        <div className='top-0 left-0 right-0 bg-first flex justify-around items-center p-2'>
+            <img src={logo} alt="" srcset="" />
+            <h1 className='w-60 h-10 bg-white flex justify-around items-center'>Home</h1>
+            <div className='flex justify-around items-center gap-5'>
+                {
+                    user.uid ? <img className='w-14 h-14 bg-slate-500 rounded-full' src="" alt="" srcset="" />
+                        :
+                        <Link className='font-bold text-teal-700'>LogIn</Link>
+                }
+                <div className='w-9 h-14 bg-slate-200 rounded-full'></div>
+
+            </div>
         </div>
     );
 };
