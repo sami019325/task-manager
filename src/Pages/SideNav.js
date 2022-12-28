f392ca9965279c2cad97a8f8fecda203bbb190c0
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-12-29 00:24:05 +0600

    side nav updated

--- src/Pages/SideNav.js
+++ src/Pages/SideNav.js
@@ -1,9 +1,40 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './../Native.css'
+import editSVG from './../resources/fi_edit-3.svg'
+import editNSVG from './../resources/fi_edit-3_black.svg'
+import myTaskSVG from './../resources/my_task.svg'
+import myTaskNSVG from './../resources/my_task_black.svg'
+import CompletedkSVG from './../resources/completed_task.svg'
+import CompletedkNSVG from './../resources/completed_task_black.svg'
+import DetailSVG from './../resources/fi_slack.svg'
+import DetailNSVG from './../resources/fi_slack_black.svg'
+import { Link } from 'react-router-dom';
 const SideNav = () => {
+    const [isMouse1, setIsMouse1] = useState(false)
+    const [isMouse2, setIsMouse2] = useState(false)
+    const [isMouse3, setIsMouse3] = useState(false)
+    const [isMouse4, setIsMouse4] = useState(false)
+    const [clickedBtn, setClickedBtn] = useState(0)
+
     return (
-        <div>
-            sider
+        <div className='bg-first relative left-0 bottom-0 w-80 p-5 flex flex-col gap-3 h-full'>
+            <Link className='task-options cursor-pointer flex items-center px-5 gap-3' onClick={() => setClickedBtn(1)} onMouseEnter={() => setIsMouse1(true)} onMouseLeave={() => setIsMouse1(false)}>
+                <img className='' src={isMouse1 || clickedBtn === 1 ? editSVG : editNSVG} alt="" srcset="" />
+                <h2 className={`${isMouse1 || clickedBtn === 1 ? 'addtask' : ''} w-full text-left font-bold`}>Add task </h2>
+            </Link>
+            <Link className='task-options cursor-pointer flex items-center px-5 gap-3' onClick={() => setClickedBtn(2)} onMouseEnter={() => setIsMouse2(true)} onMouseLeave={() => setIsMouse2(false)}>
+                <img src={isMouse2 || clickedBtn === 2 ? myTaskSVG : myTaskNSVG} alt="" srcset="" />
+                <h2 className={`${isMouse2 || clickedBtn === 2 ? 'addtask' : ''} w-full text-left font-bold `}>My Task </h2>
+            </Link>
+            <Link className='task-options cursor-pointer flex items-center px-5 gap-3' onClick={() => setClickedBtn(3)} onMouseEnter={() => setIsMouse3(true)} onMouseLeave={() => setIsMouse3(false)}>
+                <img src={isMouse3 || clickedBtn === 3 ? CompletedkSVG : CompletedkNSVG} alt="" srcset="" />
+                <h2 className={`${isMouse3 || clickedBtn === 3 ? 'addtask' : ''} w-full text-left font-bold `}>Completed task </h2>
+            </Link>
+            <Link className='task-options cursor-pointer flex items-center px-5 gap-3' onClick={() => setClickedBtn(4)} onMouseEnter={() => setIsMouse4(true)} onMouseLeave={() => setIsMouse4(false)}>
+                <img src={isMouse4 || clickedBtn === 4 ? DetailSVG : DetailNSVG} alt="" srcset="" />
+                <h2 className={`${isMouse4 || clickedBtn === 4 ? 'addtask' : ''} w-full text-left font-bold`}>Details </h2>
+            </Link>
+
         </div>
     );
 };
